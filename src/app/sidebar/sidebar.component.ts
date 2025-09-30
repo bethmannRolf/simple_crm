cab78c33d9a9a13c0a18b5e68d1997f5e07b91eb
bethmannRolf <devak.robe@gmail.com>
2025-09-30 13:09:52 +0200

    sidebar styling

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,13 +1,14 @@
 import { Component, inject, signal } from '@angular/core';
 import { ToggleSidebarService } from '../toggle-sidebar.service';
 import { UserComponent } from "../user/user.component";
+import { DashboardComponent } from "../dashboard/dashboard.component";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,         
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
-  imports: [UserComponent]
+  imports: [UserComponent, DashboardComponent, UserComponent, DashboardComponent]
 })
 export class SidebarComponent {
   private sidebarService = inject(ToggleSidebarService);
