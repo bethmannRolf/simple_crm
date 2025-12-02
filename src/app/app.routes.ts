cbf2e8e41670aeb23dcba9a969ce10c5b42429ec
bethmannRolf <devak.robe@gmail.com>
2025-12-02 15:04:54 +0100

    profile-image

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 
 import { UserContentComponent } from './user-content/user-content.component';
 import { DashboardContentComponent } from './dashboard-content/dashboard-content.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 
 export const routes: Routes = [
-    { path: '', component:DashboardContentComponent },
-    { path: 'dashboard', component: DashboardContentComponent },
-    { path: 'user', component: UserContentComponent }
+  { path: '', component: DashboardContentComponent },
+  { path: 'dashboard', component: DashboardContentComponent },
+  { path: 'user', component: UserContentComponent },
+  { path: 'users/:id', component: UserDetailComponent }  
 ];
+
