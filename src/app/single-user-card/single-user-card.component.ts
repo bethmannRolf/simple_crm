42106a537db2861bf2637a1d2497b71277623b22
bethmannRolf <devak.robe@gmail.com>
2025-12-02 10:47:23 +0100

    load users

--- src/app/single-user-card/single-user-card.component.ts
+++ src/app/single-user-card/single-user-card.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Firestore, collection } from '@angular/fire/firestore';
+import { collectionData } from '@angular/fire/firestore';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-single-user-card',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './single-user-card.component.html',
-  styleUrl: './single-user-card.component.scss'
+  styleUrls: ['./single-user-card.component.scss']
 })
 export class SingleUserCardComponent {
 
+  private firestore = inject(Firestore);
+
+  users$!: Observable<any[]>;
+
+  constructor() {
+    this.loadUsersRealtime();
+  }
+
+  loadUsersRealtime() {
+    const usersCollection = collection(this.firestore, 'users');
+    this.users$ = collectionData(usersCollection, { idField: 'id' });
+  }
 }
