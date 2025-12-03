1f22fa35e4ad43bb3ea4897f0674397f7c5e21a7
bethmannRolf <devak.robe@gmail.com>
2025-12-03 15:27:04 +0100

    get route from firestore

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore/lite';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-detail',
-  imports: [],
+  standalone: true,
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.scss'
 })
 export class UserDetailComponent {
 
+  private route = inject(ActivatedRoute);
+    private firestore = inject(Firestore);
+
+  constructor() {
+    const id = this.route.snapshot.paramMap.get('id');
+    console.log(id);
+  }
 }
